73dd16d2c2b16d9e53079057f0b2116fced005f5
letheascetic <yizhifight@163.com>
2019-05-18 16:06:42 +0800

    add hz2 nj2 spiders

--- house_analyzer/run_crawlers.py
+++ house_analyzer/run_crawlers.py
@@ -3,15 +3,19 @@
 from homelink.spiders.sx import SxSpider
 from homelink.spiders.hz import HzSpider
 from homelink.spiders.nj import NjSpider
+from homelink.spiders.nj2 import Nj2Spider
+from homelink.spiders.hz2 import Hz2Spider
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 
 
 if __name__ == '__main__':
     process = CrawlerProcess(get_project_settings())
-    process.crawl(SxSpider)
-    process.crawl(HzSpider)
-    process.crawl(NjSpider)
+    # process.crawl(SxSpider)
+    # process.crawl(HzSpider)
+    # process.crawl(NjSpider)
+    # process.crawl(Nj2Spider)
+    process.crawl(Hz2Spider)
     process.start()
     pass
 
---
+++ house_analyzer/run_crawlers.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+
+source activate py3
+nohup python run_crawlers.py & > /dev/null  2>&1
+
